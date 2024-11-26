faa275ce61cdba17936df2986ef3a1d398d43fd5
Douglas Soares de Oliveira <127028065+Dougfisica@users.noreply.github.com>
2024-11-26 10:18:56 -0300

    a

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import PhysicsSimulation from '@/components/MotionSimulator';
+import PhysicsSimulation from '@/components/PhysicsSimulation';
 
 export default function PhysicsPage() {
   return (
